a64f754929be04ac25e2a05501df040b2bc480ea
Oskar Schöldström <m@oxy.fi>
2023-05-29 10:44:08 -0300

    Support preloading video tags

--- src/Rewriters/Preload.php
+++ src/Rewriters/Preload.php
@@ -8,6 +8,7 @@ class Preload implements Rewriter
 {
     public const FILTER_SHOULD_STOP = 'wp-image-resizer/preload/should_stop';
     public const FILTER_PRELOAD_BLOCKS = 'wp-image-resizer/preload/blocks';
+    public const FILTER_CONTENT_TAGS = 'wp-image-resizer/preload/content_tags';
 
     /** @var string[] $imageBlocks */
     public array $imageBlocks = [
@@ -31,7 +32,10 @@ class Preload implements Rewriter
             while ($block = array_shift($blocks)) {
                 if (in_array($block['blockName'], $preloadBlocks)) {
                     $content = str_replace('<img ', '<img loading="eager" ', $block['innerHTML']);
+                    $content = str_replace('<video ', '<video loading="eager" ', $block['innerHTML']);
                     $content = wp_filter_content_tags($content, 'preload');
+                    $content = apply_filters(self::FILTER_CONTENT_TAGS, $content);
+
                     echo self::buildLink($content);
                 }
 
@@ -44,6 +48,28 @@ class Preload implements Rewriter
 
     public static function buildLink(string $content, string $priority = 'high'): string
     {
+        // Note not supported in chrome
+        // @see https://stackoverflow.com/a/50682498
+        if (str_contains($content, '<video')) {
+            $src = preg_match('/src="([^"]+)"/', $content, $matchSrc) ? $matchSrc[1] : '';
+            $type = match (true) {
+                str_contains($src, '.webm') => 'type/webm',
+                str_contains($src, '.mp4') => 'type/mp4',
+                default => null,
+            };
+
+            if (!$type) {
+                return '';
+            }
+
+            return sprintf(
+                '<link rel="preload" fetchpriority="%s" as="video" href="%s" type="%s">',
+                $priority,
+                $src,
+                $type,
+            );
+        }
+
         $src = preg_match('/src="([^"]+)"/', $content, $matchSrc) ? $matchSrc[1] : '';
         $srcset = preg_match('/srcset="([^"]+)"/', $content, $matchSrc) ? $matchSrc[1] : '';
         $sizes = preg_match('/sizes="([^"]+)"/', $content, $matchSrc) ? $matchSrc[1] : '';
